1b87c1cfdd0c8caf222f653b0e1a2f3f5c812387
Donald Ball <donald.ball@gmail.com>
2009-04-02 10:56:19 -0500

    parameterizing shipment with attributes like shipper, destination, etc.

--- lib/active_shipping/shipping/shipment.rb
+++ lib/active_shipping/shipping/shipment.rb
@@ -1,14 +1,20 @@
 module ActiveMerchant
   module Shipping
     class Shipment
-      attr_accessor :number, :price, :tracking, :errors
-      attr_reader :labels
+      attr_accessor :number, :price, :tracking, :errors, :shipper, :payer,
+        :origin, :destination, :service, :labels, :packages
 
       def initialize(attributes = {})
         @number = attributes[:number]
         @price = attributes[:price]
         @tracking = attributes[:tracking]
         @errors = attributes[:errors]
+        @shipper = attributes[:shipper]
+        @payer = attributes[:payer]
+        @origin = attributes[:origin]
+        @destination = attributes[:destination]
+        @packages = attributes[:packages]
+        @service = attributes[:service]
         @attributes = attributes
         @labels = []
       end
