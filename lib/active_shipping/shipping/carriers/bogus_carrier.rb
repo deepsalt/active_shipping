a6e494139fda1394808ceea2ded15f32b77479b5
Donald Ball <donald.ball@gmail.com>
2009-04-03 16:37:25 -0500

    adding buy_shipping_labels to bogus carrier

--- lib/active_shipping/shipping/carriers/bogus_carrier.rb
+++ lib/active_shipping/shipping/carriers/bogus_carrier.rb
@@ -3,14 +3,24 @@ module ActiveMerchant
     class BogusCarrier < Carrier
       cattr_reader :name
       @@name = "Bogus Carrier"
-      
-      
+
       def find_rates(origin, destination, packages, options = {})
         origin = Location.from(origin)
         destination = Location.from(destination)
         packages = Array(packages)
       end
-      
+
+      def buy_shipping_labels(shipper, origin, destination, packages, options = {})
+        tracking = Array.new(9) { rand(9) }.join
+        Shipment.new(
+          :shipper => shipper,
+          :origin => origin,
+          :destination => destination,
+          :packages => Array(packages),
+          :tracking => tracking,
+          :labels => packages.map {|p| Label.new(:tracking => tracking) }
+        )
+      end
     end
   end
 end
