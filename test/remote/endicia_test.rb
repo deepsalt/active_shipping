ec747af0775a54589f378f7439a1d665d8b2233b
Donald Ball <donald.ball@gmail.com>
2009-04-08 11:07:33 -0500

    factoring out shipper

--- test/remote/endicia_test.rb
+++ test/remote/endicia_test.rb
@@ -5,26 +5,24 @@ class EndiciaTest < Test::Unit::TestCase
     @packages = TestFixtures.packages
     @locations = TestFixtures.locations
     @carrier = Endicia.new(fixtures(:endicia))
+    @shipper = @locations[:real_google_as_commercial].dup
+    @shipper.number = '123456'
+    @shipper.passphrase = 'abcdef'
+    @shipper.attention = 'Foo Bar'
   end
 
   def test_buy_shipping_labels
     origin = @locations[:real_google_as_commercial]
-    shipper = origin.dup
-    shipper.number = '123456'
-    shipper.passphrase = 'abcdef'
-    shipper.attention = 'foobar'
     destination = @locations[:beverly_hills]
     packages = [@packages[:just_ounces], @packages[:chocolate_stuff]]
-    shipment = @carrier.buy_shipping_labels(shipper, origin, destination, packages, :test => true, :shipment_number => '987654')
+    shipment = @carrier.buy_shipping_labels(@shipper, origin, destination, packages, :test => true, :shipment_number => '987654')
     assert shipment.errors.empty?
     assert_equal shipment.packages.length, shipment.labels.length
   end
 
   def test_change_passphrase
-    shipper = @locations[:real_google_as_commercial].dup
-    shipper.number = '123456'
-    shipper.passphrase = 'abcdef'
-    @carrier.change_passphrase(shipper)
-    assert_not_equal 'abcdef', shipper.passphrase
+    old = @shipper.passphrase
+    @carrier.change_passphrase(@shipper)
+    assert_not_equal old, @shipper.passphrase
   end
 end
